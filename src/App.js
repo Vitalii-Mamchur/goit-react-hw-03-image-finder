2d7f0647f7c2921cbe919224d24cac0bc8b42b5f
Vitalii-Mamchur <65975103+Vitalii-Mamchur@users.noreply.github.com>
2021-06-21 16:17:17 +0300

    add functional

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import Loader from "react-loader-spinner";
 import Searchbar from "./components/Searchbar";
 import ImageGallery from "./components/ImageGallery";
 
@@ -7,28 +8,57 @@ class App extends Component {
   state = {
     hits: [],
     currentPage: 1,
+    searchQuery: "",
+    isLoading: false,
+    error: null,
   };
 
-  componentDidMount() {}
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchQuery !== this.state.searchQuery) {
+      this.fetchHits();
+    }
+  }
 
   onChangeQuery = (query) => {
-    const { currentPage } = this.state;
+    this.setState({ hits: [], searchQuery: query, currentPage: 1 });
+  };
+
+  fetchHits = () => {
+    const { currentPage, searchQuery } = this.state;
+
+    this.setState({ isLoading: true });
+
     axios
       .get(
-        `https://pixabay.com/api/?q=${query}&key=19964688-39e6ce365709953a823cd9b18&image_type=photo&orientation=horizontal&per_page=12&page=${currentPage}`
+        `https://pixabay.com/api/?q=${searchQuery}&key=19964688-39e6ce365709953a823cd9b18&image_type=photo&orientation=horizontal&per_page=12&page=${currentPage}`
       )
       .then((res) => {
         // console.log(res.data.hits);
-        this.setState({ hits: res.data.hits });
-      });
-    console.log(query);
+        this.setState((prevState) => ({
+          hits: [...prevState.hits, ...res.data.hits],
+          currentPage: prevState.currentPage + 1,
+        }));
+      })
+      .catch((error) => this.setState({ error }))
+      .finally(() => this.setState({ isLoading: false }));
   };
 
   render() {
+    const { hits, isLoading, error } = this.state;
+    const shouldRenderLoadMoreButton = hits.length > 0 && !isLoading;
     return (
       <>
+        {error && alert("Oooops")}
         <Searchbar onSubmit={this.onChangeQuery} />
-        <ImageGallery hits={this.state.hits} />
+        <ImageGallery hits={hits} />
+        {isLoading && (
+          <Loader type="ThreeDots" color="#000000" height={80} width={80} />
+        )}
+        {shouldRenderLoadMoreButton && (
+          <button tupe="button" onClick={this.fetchHits}>
+            Load more
+          </button>
+        )}
       </>
     );
   }
